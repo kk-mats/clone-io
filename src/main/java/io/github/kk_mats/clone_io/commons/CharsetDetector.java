ea9432897b814e47588f80a9c38a7afb8acd1b57
k-matusm <k-matusm@sel-samba.ics.es.osaka-u.ac.jp>
2019-07-03 17:48:53 +0900

    add charset detection

---
+++ src/main/java/io/github/kk_mats/clone_io/commons/CharsetDetector.java
@@ -0,0 +1,33 @@
+package io.github.kk_mats.clone_io.commons;
+
+
+import org.mozilla.universalchardet.UniversalDetector;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+
+
+public class CharsetDetector
+{
+	private static final int block_size=4096;
+	private static byte[] buffer=new byte[block_size];
+	private static UniversalDetector detector=new UniversalDetector(null);
+
+	public static Charset detect(final Path file) throws NullPointerException, IllegalArgumentException, IOException
+	{
+		detector.reset();
+		var is=Files.newInputStream(Objects.requireNonNull(file), StandardOpenOption.READ);
+		int loaded;
+		while((loaded=is.read(buffer))>0 && !detector.isDone())
+		{
+			detector.handleData(buffer, 0, loaded);
+		}
+
+		detector.dataEnd();
+		return Charset.forName(detector.getDetectedCharset());
+	}
+}
